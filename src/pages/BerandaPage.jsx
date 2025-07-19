6c5dbbf3c4c019b613f1f4bdfa802a507549c14d
Riyanto24434 <aris.riyanto24434@gmail.com>
2025-07-19 10:12:39 +0000

    fix: tambahkan import useRef agar dropdown tidak error

--- src/pages/BerandaPage.jsx
+++ src/pages/BerandaPage.jsx
@@ -1,5 +1,5 @@
 import "@/styles/beranda.css";
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const courseData = [
